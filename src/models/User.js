f69b177f148e446f19f90e827c344cbd95cfc9a0
Kataleko <katalekoweb@gmail.com>
2026-04-03 17:42:28 +0100

    feat: implementing bcrypt for user password

--- src/models/User.js
+++ src/models/User.js
@@ -1,12 +1,17 @@
 import mongoose from 'mongoose';
+import bcrypt from "bcrypt"
 
 const UserSchema = new mongoose.Schema({
     name: {type: String, required: true},
     username: {type: String, required: true, unique: true},
     email: {type: String, required: true, unique: true},
-    password: {type: String, required: true},
+    password: {type: String, required: true, select: false},
     avatar: {type: String},
     background: {type: String}
+})
+
+UserSchema.pre("save", async function () {
+    this.password = await bcrypt.hash(this.password, 10);
 })
 
 const User = mongoose.model("User", UserSchema)
