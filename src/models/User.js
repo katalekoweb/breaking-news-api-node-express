0928a48b56b6dbb2dacc757e86f37c26744cce84
Kataleko <katalekoweb@gmail.com>
2026-04-03 17:46:48 +0100

    refactor: set email and username to lowercase in model

--- src/models/User.js
+++ src/models/User.js
@@ -3,8 +3,8 @@ import bcrypt from "bcrypt"
 
 const UserSchema = new mongoose.Schema({
     name: {type: String, required: true},
-    username: {type: String, required: true, unique: true},
-    email: {type: String, required: true, unique: true},
+    username: {type: String, required: true, unique: true, lowercase: true},
+    email: {type: String, required: true, unique: true, lowercase: true},
     password: {type: String, required: true, select: false},
     avatar: {type: String},
     background: {type: String}
