4a181f6181ed121696a529a5dc2bbc50822ec5ab
Kataleko <katalekoweb@gmail.com>
2026-04-06 10:16:49 +0100

    feat: posts pagination

--- src/controllers/post.controller.js
+++ src/controllers/post.controller.js
@@ -3,11 +3,46 @@ import mongoose from "mongoose";
 
 const getAll = async (req, res) => {
   try {
-    const posts = await postService.getAll();
+    let {limit, offset } = req.query;
+
+    limit = Number(limit) || 3
+    offset = Number(offset) || 0
+
+    const posts = await postService.getAll(limit, offset)
+    const total = await postService.countDocuments()
+    const currentUrl = req.baseUrl
+
+    const next = offset + limit
+    const nextUrl = next < total ? `${currentUrl}?limit=${limit}&offset=${next}` : null;
+
+    const previous = offset - limit >= 0 ? offset - limit : null
+    const previousUrl = previous !== null ? `${currentUrl}?limit=${limit}&offset=${previous}` : null;
 
     if (posts.length === 0) return res.status(404).send({ message: "No posts found" });
 
-    res.status(200).send(posts);
+    res.status(200).send({
+      nextUrl,
+      previousUrl,
+      total,
+      offset,
+      limit,
+      results: posts.map(post => ({
+        id: post._id,
+        title: post.title,
+        text: post.text,
+        banner: post.banner,
+        user: {
+          id: post.user._id,
+          name: post.user.name,
+          username: post.user.username,
+          email: post.user.email,
+          avatar: post.user.avatar
+        },
+        likes: post.likes,
+        comments: post.comments,
+        createdAt: post.createdAt
+      }))
+    });
   } catch (error) {
     res.status(500).send({ message: "Error: ", error: error.message });
   }
--- src/services/post.service.js
+++ src/services/post.service.js
@@ -1,6 +1,8 @@
 import Post from "../models/Post.js"
 
-const getAll = () => Post.find()
+const getAll = (limit, offset) => Post.find().sort({_id: -1}).skip(offset).limit(limit).populate("user")
+
+const countDocuments = () => Post.countDocuments()
 
 const create = (body) => Post.create(body)
 
@@ -8,4 +10,4 @@ const update = (id, body) => Post.findByIdAndUpdate(id, body, { new: true })
 
 const deletePost = (id) => Post.findByIdAndDelete(id)
 
-export default { create, getAll, update, delete: deletePost }
+export default { create, getAll, countDocuments, update, delete: deletePost }
