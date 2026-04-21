9c8d191e7c1a2423875aa3969d2b18dc22daa85c
Kataleko <katalekoweb@gmail.com>
2026-04-21 20:30:44 +0100

    test errors

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -6,16 +6,16 @@ const authMiddleware = (req, res, next) => {
     const { authorization } = req.headers;
 
     if (!authorization)
-      return res.status(401).send({ message: "Unauthorized" });
+      return res.status(401).send({ message: "Unauthorized 1" });
 
     const parts = authorization.split(" ");
     const [schema, token] = parts;
 
     if (schema !== "Bearer")
-      return res.status(401).send({ message: "Unauthorized" });
+      return res.status(401).send({ message: "Unauthorized 2" });
 
     if (parts.length !== 2)
-      return res.status(401).send({ message: "Unauthorized" });
+      return res.status(401).send({ message: "Unauthorized 3" });
 
     jwt.verify(token, process.env.JWT_SECRET, async (err, decoded) => {
       if (err) return res.status(401).send({ message: "Invalid token"});
